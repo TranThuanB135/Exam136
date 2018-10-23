758e214f5cb0fce53119c0bd39c7a953acdab575
dell <dell@DESKTOP-1B20FBB>
2018-10-23 19:55:22 +0700

    NewProject

---
+++ ASPMVC/ASPMVC/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace ASPMVC.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ ASPMVC/ASPMVC/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace ASPMVC.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
